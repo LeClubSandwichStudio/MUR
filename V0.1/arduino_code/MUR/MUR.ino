2a2bd1720539671cd570c92b2c86b9f90050c5d3
Kenny <40118685+KennnyTroll@users.noreply.github.com>
2020-03-29 20:35:37 +0200

    Visual Studio Code Update

--- V0.1/arduino_code/MUR/MUR.ino
+++ V0.1/arduino_code/MUR/MUR.ino
@@ -1,12 +1,9 @@
 /***************************************************************************
   Copyright 2020 LE CLUB SANDWICH STUDIO SAS
-
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
-
   http://www.apache.org/licenses/LICENSE-2.0
-
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +16,16 @@
   ALARMS ARE NOT JET WORKING PROPERLY DUE TO TWO REASONS :
   - BME280 library is blocking the loop if the sensor fails
   - I didn't untangle all the program flow...
-
   BUT :
   It beeps when you exceed peakAlarmLevel and plateauAlarmlevel
   This provides a minimum of over pressure protection...
 ***************************************************************************/
 
 
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
 #include <Servo.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
 Servo inValve;
